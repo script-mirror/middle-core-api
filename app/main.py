aa8379d067cd4bc97a3004ca307b2c3e8886ea26
arthurrfmoraesrz <arthur.moraes@raizen.com>
2025-02-11 15:22:28 -0300

    feat: executando com parametros; reestruturando codigo; page load timeout para 10s

--- app/main.py
+++ app/main.py
@@ -1,9 +1,10 @@
 import os
 import pdb
 import json
+import time
 import locale
 import datetime
-from time import sleep
+import argparse
 from typing import List, Dict
 from dotenv import load_dotenv
 from selenium import webdriver
@@ -17,9 +18,17 @@ from utils import *
 from airflow import trigger_airflow_dag
 locale.setlocale(locale.LC_ALL, 'pt_BR.utf-8')
 
-import time
+import logging
+logging.basicConfig(level=logging.INFO,
+                    format='%(levelname)s:\t%(asctime)s\t %(name)s.py:%(lineno)d\t %(message)s',
+                    datefmt='%Y-%m-%d %H:%M:%S',
+                    handlers=[
+                        logging.StreamHandler()
+                    ])
 
-PATH_DOWNLOAD = "/WX2TB/Documentos/fontes/PMO/scripts_unificados/apps/webhook/arquivos/tmp"
+load_dotenv()
+
+PATH_DOWNLOAD = os.getenv("PATH_DOWNLOAD")
 
 def get_products():
     with open('produtos-sintegre.json', encoding="UTF8") as products_file:
@@ -65,88 +74,62 @@ def download_products(driver:webdriver.Chrome, products: List[dict], product_dat
         path_arquivo = f'{PATH_DOWNLOAD}{url[url.rfind("/"):]}'
         paths_download.append(path_arquivo)
         
-        driver.set_page_load_timeout(1)
+        driver.set_page_load_timeout(10)
         try:
+            print('\n')
             driver.get(url)
-            print(url)
+            logging.info(url)
             if '.txt' in url:
-                sleep(1)
+                time.sleep(1)
                 txt:str = driver.page_source[driver.page_source.index('pre-wrap;">')+11:driver.page_source.index('</pre></body></html>')]
                 with open(path_arquivo, "w", encoding="utf-8") as file:
                     file.write(txt)
                 
-            sleep(1)
-            print(f"Download iniciado para {url}")
+            time.sleep(1)
+            logging.info(f"\033[92mDownload iniciado para {product['name']}\033[0m")
             webhook_payload.append({
-                "origem": "botSintegre",
+                "function_name":product['funcName'],
+                "product_details":
+                {"origem": "botSintegre",
                 "dataProduto": (product_date + (datetime.timedelta(days=product['dateDiff'][-1]))).strftime("%d/%m/%Y"),
                 "nome": product["name"],
                 "url": path_arquivo,
-                "enviar":False
+                "enviar":False}
             })
             
         except Exception as e:
+            logging.warning(f"\033[91mErro no produto {product['name']}\033[0m")
+            
             failed.append(product)
         # if not os.path.exists(path_arquivo):
     return webhook_payload
 
     
-def send_to_webhook(paths_download:List[str]) -> None:
-    
-    print("-"*60)
-    for product in paths_download:
-        if os.path.exists(product['url']):
-            res = trigger_airflow_dag("WEEBHOOK", {"product_details":product})
+def send_to_webhook(airflow_products:List[str]) -> None:
+    logging.info("-"*60)
+    pdb.set_trace()
+    for product in airflow_products:
+        if os.path.exists(product['product_details']['url']):
+            res = trigger_airflow_dag("WEBHOOK", product)
             if res:
-                print(f"Produto {product['nome']} enviado para o webhook")
+                logging.info(f"Produto {product['product_details']['nome']} enviado para o webhook")
             else:
-                print(f"Erro ao enviar para o webhook {product['nome']}")
+                logging.info(f"Erro ao enviar para o webhook {product['product_details']['nome']}")
         else:
-            print(product)
-            print('produto nao encontrado')
-            print("-"*60)
+            logging.info(product)
+            logging.info('produto nao encontrado')
+            logging.info("-"*60)
 
-def is_download_complete(download_dir):
+
+def is_download_complete(download_dir:str) -> bool:
     for filename in os.listdir(download_dir):
         if filename.endswith('.crdownload'):
             return False
     return True   
 
-def main() -> None:
-    load_dotenv()
-    __email = os.getenv("EMAIL")
-    __password = os.getenv("PASSWORD")
-    print("init")
-    driver = initialize_driver()
-    print("Realizando login")
-    login(driver, __email, __password)
+def get_url_datetime_pattern(products:List[dict], product_date:datetime.date) -> List[dict]:
 
-    produtos = get_products()
-    product_date = datetime.date(2025, 2, 6)
-    produtos = get_url_datetime_pattern(produtos, product_date=product_date)
-
-    print("iniciando downloads")
-
-    paths_download:List[dict] = download_products(driver, produtos, product_date=product_date)
-    timeout = 300
-    start_time = time.time()
-
-    while not is_download_complete(PATH_DOWNLOAD):
-        print("Download em andamento...")    
-        if time.time() - start_time > timeout:
-            raise Exception("O download não foi concluído dentro do tempo limite.")
-        time.sleep(1)
-    driver.quit()
-    print("Enviando para o webhook")
-    send_to_webhook(paths_download)
-    print("Fim")
-
-
-
-
-def get_url_datetime_pattern(produtos, product_date:datetime.date) -> List[dict]:
-
-    for prod in produtos:
+    for prod in products:
         date_params = []
         for date_diff in prod['dateDiff']:
             if prod['dateDiffUnit'] == 'day':
@@ -158,7 +141,7 @@ def get_url_datetime_pattern(produtos, product_date:datetime.date) -> List[dict]
         if prod['baseUrl'].format(*url_params) != "":
             prod['baseUrl'] = prod['baseUrl'].format(*url_params)
     
-    return produtos
+    return products
 
 def elec_date_to_str(date:datetime.date, format:str) -> str:
     elec_date_str = ""
@@ -180,9 +163,65 @@ def elec_date_to_str(date:datetime.date, format:str) -> str:
         elec_date_str = f"{date.strftime('%m%Y_rv')}{rev}d{str(date.day).zfill(2)}"
 
     return elec_date_str
+
+def execute_bot(product_date:datetime.date = datetime.date.today(), trigger_webhook:bool = False, download_timeout:int = 300) -> None:
+    __email = os.getenv("EMAIL")
+    __password = os.getenv("PASSWORD")
+
+    logging.info("Limpando pasta de produtos...")
+    os.system(f'rm -r {PATH_DOWNLOAD}/*')
+    
+    driver = initialize_driver()
+    logging.info("Realizando login")
+    login(driver, __email, __password)
+
+    produtos = get_url_datetime_pattern(get_products(), product_date=product_date)
+
+    logging.info("Iniciando downloads")
+
+    airflow_products:List[dict] = download_products(driver, produtos, product_date=product_date)
+
+    start_time = time.time()
+    while not is_download_complete(PATH_DOWNLOAD):
+        logging.info(f"Download em andamento... {time.time() - start_time:.2f}s")
+   
+        if time.time() - start_time > download_timeout:
+            raise Exception("O download não foi concluído dentro do tempo limite.")
+        time.sleep(1)
+    driver.quit()
+    if trigger_webhook:
+        logging.info("Enviando para o webhook")
+        send_to_webhook(airflow_products)
+    logging.info("Fim")
+
+
+
+
+
+def main() -> None:
+    print("python main.py --help\n")
+    parser = argparse.ArgumentParser(description='Execute o bot com os seguintes parametros.')
+    parser.add_argument('--product_date', type=lambda s: datetime.datetime.strptime(s, '%Y-%m-%d').date(), default=datetime.date.today(), help='Product date no formato YYYY-MM-DD')
+    parser.add_argument('--no_trigger', action='store_false', help='Nao triga webhook')
+    parser.add_argument('--download_timeout', type=int, default=300, help='Download timeout maximo em segundos')
+
+    args = parser.parse_args()
+    execute_bot(product_date=args.product_date, trigger_webhook=args.trigger_webhook, download_timeout=args.download_timeout)
+
     
     
 
     
 if __name__ == "__main__":
+
+    # send_to_webhook(    [{
+    #     "function_name": "arquivo_acomph",
+    #     "product_details": {
+    #         "origem": "botSintegre",
+    #         "dataProduto": "09/02/2025",
+    #         "nome": "Acomph",
+    #         "url": "/WX2TB/Documentos/fontes/PMO/scripts_unificados/apps/webhook/arquivos/tmp2/ACOMPH_09.02.2025.xls",
+    #         "enviar": True
+    #     }
+    # }])
     main()

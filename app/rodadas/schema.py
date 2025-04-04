7469893e279de8c0b04f4a07dabe37ab0343613c
arthurrfmoraesrz <arthur.moraes@raizen.com>
2025-04-04 16:05:38 -0300

    feat: valor default para id_dataviz

--- app/rodadas/schema.py
+++ app/rodadas/schema.py
@@ -6,7 +6,7 @@ class RodadaSmap(BaseModel):
     dt_rodada: datetime.date
     hr_rodada: int
     str_modelo: str
-    id_dataviz_chuva: str
+    id_dataviz_chuva: str = Field(default="")
     prev_estendida: bool = Field(default=False)
     
 class RodadaCriacao(BaseModel):

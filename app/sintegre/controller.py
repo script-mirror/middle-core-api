945ab8ea79a55436130e04cfc37ff4d5086f05cf
arthurrfmoraesrz <arthur.moraes@raizen.com>
2025-04-15 15:09:34 -0300

    feat: service do app sintegre padronizado

--- app/sintegre/controller.py
+++ app/sintegre/controller.py
@@ -3,44 +3,43 @@ from typing import List, Optional
 import datetime
 from .schema import *  
 
-from app.sintegre.service import ProductService
+from . import service
 
 router = APIRouter(prefix="/bot-sintegre", tags=["Bot Sintegre"])
-product_service = ProductService()
 
 @router.post("/trigger")
 def trigger_products(productDate:datetime.date, triggerWebhook:Optional[bool]=True):
-    return product_service.trigger(productDate, triggerWebhook)
+    return service.Product.trigger(productDate, triggerWebhook)
 
 @router.post("/trigger/{product_id}")
 def trigger_by_id(product_id: int, productDate:datetime.date):
-    return product_service.trigger_by_id(productDate,product_id)
+    return service.Product.trigger_by_id(productDate,product_id)
 
 @router.get("/json/{product_id}")
 def get_product_by_id(product_id: int):
-    product = product_service.get_product_by_id(product_id)
+    product = service.Product.get_product_by_id(product_id)
     if not product:
         raise HTTPException(status_code=404, detail="Produto nao encontrado")
     return product
 
 @router.get("/json")
 def get_products_json():
-    return product_service.get_products_json()
+    return service.Product.get_products_json()
 
 @router.get("")
 def get_products():
-    return product_service.get_products()
+    return service.Product.get_products()
 
 
 @router.put("/{product_id}")
 def update_product(product_id: int, last_received: str):
-    return product_service.update_product(product_id, last_received)
+    return service.Product.update_product(product_id, last_received)
 
 
 @router.post("/verify")
 def verify_if_is_new(
     product_details:ProductUpdate
     ):
-    return product_service.verify_if_is_new(
+    return service.Product.verify_if_is_new(
         product_details
     )
--- app/sintegre/service.py
+++ app/sintegre/service.py
@@ -6,47 +6,51 @@ from app.core.database.wx_dbClass import db_mysql_master
 from .schema import *
 from ..core.utils import text
 
-class ProductService:
-    __DB__:db_mysql_master
-    tb:db.Table    
-    def __init__(self):
-        self.__DB__ = db_mysql_master('bot_sintegre')
-        self.tb = self.__DB__.getSchema('products')
-    def trigger(self, product_date:datetime.date, trigger_webhook:bool):
+__DB__:db_mysql_master = db_mysql_master('bot_sintegre')
+class Product:
+    tb = __DB__.getSchema('products')
+    @staticmethod
+    def trigger( product_date:datetime.date, trigger_webhook:bool):
         return trigger_bot(product_date, trigger_webhook=trigger_webhook)
 
-    def trigger_by_id(self, product_date: datetime.date, product_id: int):
+    @staticmethod
+    def trigger_by_id( product_date: datetime.date, product_id: int):
         return trigger_bot(product_date, product_id)
-
-    def get_products_json(self):
+    @staticmethod
+    def get_products_json():
+        
         return get_products(None)
 
-    def get_product_by_id(self, product_id: int):
+    @staticmethod
+    def get_product_by_id( product_id: int):
         return get_products(product_id)
     
-    def get_products(self):
+    @staticmethod
+    def get_products():
         query = db.select(
-            self.tb.c['id'],
-            self.tb.c['name'],
-            self.tb.c['last_received'],
-            self.tb.c['updated_at']
+            Product.tb.c['id'],
+            Product.tb.c['name'],
+            Product.tb.c['last_received'],
+            Product.tb.c['updated_at']
         )
-        result = self.__DB__.db_execute(query, commit=True).fetchall()
+        result = Product.__DB__.db_execute(query, commit=True).fetchall()
         df = pd.DataFrame(result, columns=['id', 'name', 'last_received', 'updated_at'])
         return df.to_dict('records')
         
-    def update_product(self, product_id: int, last_received: str):
-        query = db.update(self.tb).values({'updated_at':datetime.datetime.now(), 'last_received':last_received}).where(self.tb.c['id'] == product_id)
-        self.__DB__.db_execute(query, commit=True)
+    @staticmethod
+    def update_product( product_id: int, last_received: str):
+        query = db.update(Product.tb).values({'updated_at':datetime.datetime.now(), 'last_received':last_received}).where(Product.tb.c['id'] == product_id)
+        Product.__DB__.db_execute(query, commit=True)
         return True
     
-    def verify_if_is_new(self, product_details:ProductUpdate):
+    @staticmethod
+    def verify_if_is_new( product_details:ProductUpdate):
         product_details = product_details.dict()
-        products = self.get_products()
+        products = Product.get_products()
         product = [x for x in products if x['name'] == text.remover_acentos_caracteres_especiais(product_details['nome'])][0]
         if product['last_received'] == product_details['fileHash']:
             return False
-        self.update_product(product['id'], product_details['fileHash'])
+        Product.update_product(product['id'], product_details['fileHash'])
         return True
     
     
